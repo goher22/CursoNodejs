59638bc58f6da33c0d931b6690f5ad77199f4b77
Carlos <gohercarlos@gmail.com>
2021-08-15 23:40:19 -0500

    const, var and let

---
+++ 02-fundamentos/const-var-let.js
@@ -0,0 +1,16 @@
+var nombre = 'Wolverine'; //Crea esta bariable en ambito global
+const nombre3 = 'Wolverine'; // Variable inmutable
+
+if(true){
+    var nombre = 'Magneto';
+}
+
+let nombre2 = 'Wolverine'; //Crea esta bariable en ambito global
+
+if(true){
+    let nombre2 = 'Magneto';
+    //console.log(nombre2)
+}
+
+console.log(nombre)
+console.log(nombre2)
