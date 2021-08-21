2ef4cb00c4793db990940e61545ef9e7f688000e
Carlos <gohercarlos@gmail.com>
2021-08-21 18:37:05 -0500

    Guardar archivo

--- 03-base-node/app.js
+++ 03-base-node/app.js
@@ -1,8 +1,19 @@
+const fs = require('fs')
+
 console.clear()
+const base = 4
 console.log("=========================")
-console.log("       TABLA DEL 5       ")
+console.log(`       TABLA DEL ${base}       `)
 console.log("=========================")
-const base = 5
+
+let salida = ''
 for(let i = 1; i <= 10; i++){
-  console.log(`5x${i}=${base*i}`);
+  salida += `${base}X${i}=${base*i}\n`
 }
+
+console.log(salida)
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if(err) throw err;
+    console.log(`tabla-${base}.txt creado`);
+})
