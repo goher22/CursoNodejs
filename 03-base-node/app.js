3040d78a943a3628d0fb8fafa0d5d7e36a792878
Carlos <gohercarlos@gmail.com>
2021-08-21 18:18:41 -0500

    Tabla del 5

---
+++ 03-base-node/app.js
@@ -0,0 +1,8 @@
+console.clear()
+console.log("=========================")
+console.log("       TABLA DEL 5       ")
+console.log("=========================")
+const base = 5
+for(let i = 1; i <= 10; i++){
+  console.log(`5x${i}=${base*i}`);
+}
