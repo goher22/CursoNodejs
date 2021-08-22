5d2c08dd0548621c8db2c1d2919638c5ddd6ec54
Carlos <gohercarlos@gmail.com>
2021-08-22 01:21:59 -0500

    yargs

--- 03-base-node/app.js
+++ 03-base-node/app.js
@@ -1,10 +1,14 @@
-const {creararchivo} = require('./helpers/multiplicar')
+//const {creararchivo} = require('./helpers/multiplicar')
+const argv = require('yargs').argv
 console.clear()
 
-const [,,arg3 = 'base=5'] = process.argv
-const [,base = 5] = arg3.split('=')
+console.log(process.argv)
+console.log("base yargs",argv.base)
 
-creararchivo(base)
-    .then(nombreArchivo=> console.log(nombreArchivo, 'creado'))
-    .catch(err => console.log(err))
+// const [,,arg3 = 'base=5'] = process.argv
+// const [,base = 5] = arg3.split('=')
+
+// creararchivo(base)
+//     .then(nombreArchivo=> console.log(nombreArchivo, 'creado'))
+//     .catch(err => console.log(err))
 
