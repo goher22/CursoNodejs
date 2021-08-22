3991208420b832ee9fd8db905c67d2a545745b2e
Carlos <gohercarlos@gmail.com>
2021-08-21 21:59:39 -0500

    recivir argumento desde a consola

--- 03-base-node/app.js
+++ 03-base-node/app.js
@@ -1,6 +1,9 @@
 const {creararchivo} = require('./helpers/multiplicar')
 console.clear()
-const base = 4
+
+const [,,arg3 = 'base=5'] = process.argv
+const [,base = 5] = arg3.split('=')
+
 creararchivo(base)
     .then(nombreArchivo=> console.log(nombreArchivo, 'creado'))
     .catch(err => console.log(err))
