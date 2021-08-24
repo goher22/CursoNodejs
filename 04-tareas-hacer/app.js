883c15bfa192987aa2098a31a619c6e2eb9fa17d
Carlos <gohercarlos@gmail.com>
2021-08-24 01:18:21 -0500

    stdin -stdout

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,8 +1,16 @@
 require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 console.clear()
 
 const main = async () => {
     console.log('Hola mundo')
+
+    mostrarMenu()
+
+    //pausa()
+
 }
 
 main()
