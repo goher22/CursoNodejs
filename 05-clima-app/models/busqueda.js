9f5eb7696394412a6463c062cbf4421aca315fb9
Carlos <gohercarlos@gmail.com>
2021-09-05 18:45:28 -0500

    Axios

--- 05-clima-app/models/busqueda.js
+++ 05-clima-app/models/busqueda.js
@@ -8,10 +8,24 @@ class Busquedas {
 
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiZ29oZXIiLCJhIjoiY2t0N3R0NXo2MHZtbDJ2bnI5ZGlmZDhvayJ9.bFSSk3S3KgTZ-q3-wq36-g',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async ciudad(lugar=''){
         //console.log('Ciudad:', lugar)
         try {
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Sinc.json?access_token=pk.eyJ1IjoiZ29oZXIiLCJhIjoiY2t0N3R0NXo2MHZtbDJ2bnI5ZGlmZDhvayJ9.bFSSk3S3KgTZ-q3-wq36-g&limit=5&language=es')
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await instance.get()
+            //const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Sinc.json?access_token=pk.eyJ1IjoiZ29oZXIiLCJhIjoiY2t0N3R0NXo2MHZtbDJ2bnI5ZGlmZDhvayJ9.bFSSk3S3KgTZ-q3-wq36-g&limit=5&language=es')
             console.log(resp.data)
             return []
         }catch (error) {
