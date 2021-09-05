d65f180801b2a192003cc93fcd4664560122d5ac
Carlos <gohercarlos@gmail.com>
2021-09-05 18:23:09 -0500

    Mapbox

--- 05-clima-app/models/busqueda.js
+++ 05-clima-app/models/busqueda.js
@@ -11,7 +11,7 @@ class Busquedas {
     async ciudad(lugar=''){
         //console.log('Ciudad:', lugar)
         try {
-            const resp = await axios.get('https://reqres.in/api/users?page=2')
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Sinc.json?access_token=pk.eyJ1IjoiZ29oZXIiLCJhIjoiY2t0N3R0NXo2MHZtbDJ2bnI5ZGlmZDhvayJ9.bFSSk3S3KgTZ-q3-wq36-g&limit=5&language=es')
             console.log(resp.data)
             return []
         }catch (error) {
