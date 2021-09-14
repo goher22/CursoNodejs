ab2e0f750fd18b0ae641ccd032b7574dcf0b7ceb
Carlos <gohercarlos@gmail.com>
2021-09-14 00:52:13 -0500

    servir un sitio web completo

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,21 +2,34 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
-// Servir contenido estático
+// // Servir contenido estático
 app.use(express.static('public'));
+
+app.get('/elements', (req, res) =>{
+  res.sendFile(__dirname+'/public/elements.html')
+  //res.send('Elements Page');
+})
+app.get('/generic', (req, res) =>{
+  res.sendFile(__dirname+'/public/generic.html')
+  //res.send('Generic Page');
+})
+app.get('/', (req, res) =>{
+  res.sendFile(__dirname+'/public/index.html')
+  //res.send('Home Page');
+})
  
 // app.get('/', (req, res) => {
 //   res.send('Home Page')
 // })
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hello World')
-})
+// app.get('/hola-mundo', (req, res) => {
+//     res.send('Hello World')
+// })
 
-app.get('*', (req, res) => {
-    //res.sendFile(__dirname+'public/404.html')
-    res.send('404  Page not found')
-})
+// app.get('*', (req, res) => {
+//     //res.sendFile(__dirname+'/public/404.html')
+//     res.send('404  Page not found')
+// })
  
 app.listen(port, (() => {
   console.log(`Exapmle app listening at http://localhost:${port}`)
