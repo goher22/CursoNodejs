c3580dacca64d66d040fb0ea05cfce4c9be79a54
Carlos <gohercarlos@gmail.com>
2021-09-14 00:09:18 -0500

    introduccion de express

---
+++ 06-webserver/app-opd.js
@@ -0,0 +1,28 @@
+const http = require('http')
+
+http.createServer((req, res) => {
+    //console.log(req)
+    //res.writeHead(404)
+    //res.writeHead(200, {'Content-Type': 'text/plain'})
+    // res.writeHead(200, {'Content-Type': 'application/json'})
+    //crea un archivo de csv
+    // res.setHeader('Content-Disposition', 'attachment; filename=lista.csv' )
+    // res.writeHead(200, {'Content-Type': 'application/csv'})
+
+    // const persona = {
+    //     id: 1,
+    //     nombre: 'Fernando'
+    // }
+    // res.write(JSON.stringify(persona))
+    // Informacion del archivo
+    // res.write('id', 'nombre')
+    // res.write('1', 'Marina')
+    // res.write('2', 'Isaac')
+    // res.write('3', 'Juan')
+    // res.write('4', 'Carlos')
+    res.write('Hola Mundo')
+    res.end()
+})
+.listen(8080)
+
+console.log('Escuchando el puerto', 8080)
--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,27 +1,16 @@
-const http = require('http')
-
-http.createServer((req, res) => {
-    //console.log(req)
-    //res.writeHead(404)
-    //res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    //crea un archivo de csv
-    // res.setHeader('Content-Disposition', 'attachment; filename=lista.csv' )
-    // res.writeHead(200, {'Content-Type': 'application/csv'})
-
-    const persona = {
-        id: 1,
-        nombre: 'Fernando'
-    }
-    res.write(JSON.stringify(persona))
-    // Informacion del archivo
-    // res.write('id', 'nombre')
-    // res.write('1', 'Marina')
-    // res.write('2', 'Isaac')
-    // res.write('3', 'Juan')
-    // res.write('4', 'Carlos')
-    res.end()
+const express = require('express')
+const app = express()
+ 
+app.get('/', (req, res) => {
+  res.send('Home Page')
 })
-.listen(8080)
 
-console.log('Escuchando el puerto', 8080)
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('*', (req, res) => {
+    res.send('404  Page not found')
+})
+ 
+app.listen(8080)
