806164e30368e07fd30b6256f653ab49efe0b52a
Carlos <gohercarlos@gmail.com>
2021-09-14 00:27:33 -0500

    Servir contenido estatico

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const app = express()
+const port = 8080;
+
+// Servir contenido estático
+app.use(express.static('public'));
  
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
+// app.get('/', (req, res) => {
+//   res.send('Home Page')
+// })
 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hello World')
 })
 
 app.get('*', (req, res) => {
+    //res.sendFile(__dirname+'public/404.html')
     res.send('404  Page not found')
 })
  
-app.listen(8080)
+app.listen(port, (() => {
+  console.log(`Exapmle app listening at http://localhost:${port}`)
+}))
