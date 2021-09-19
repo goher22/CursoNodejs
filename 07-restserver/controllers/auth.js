ac88af6ede852c1af957b5905d7f087a9584ae1b
Carlos <gohercarlos@gmail.com>
2021-09-19 01:07:32 -0500

    Login de usuario

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -1,10 +1,47 @@
 const { response } = require("express");
+const Usuario = require('../models/usuarios')
+const bcryptjs = require('bcryptjs')
 
-const login = (req, res = response) => {
+const login = async (req, res = response) => {
 
-    res.json({
-        msg: 'Login ok'
-    })
+    const {correo, password} = req.body
+
+    try{
+        //Verificar si el email existe
+        const usuario = await Usuario.findOne({correo})
+        if(!usuario){
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            })
+        }
+
+        //Si el usuario esta activo
+        if (!usuario.estado){
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            })
+        }
+
+        //Verificar la contraseña
+        const validaPassword = bcryptjs.compareSync(password, usuario.password)
+
+        if(!validaPassword){
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            })
+        }
+
+        //Generar el JWT
+
+
+        res.json({
+            msg: 'Login ok'
+        })
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({msg: "Hable con el adminstrador"})
+    }
 
 }
 
