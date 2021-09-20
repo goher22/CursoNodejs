d048e9721116ee8d24632ada38c61ecf7b9beff8
Carlos <gohercarlos@gmail.com>
2021-09-20 01:23:13 -0500

    Buscar por otros argumentos

--- 07-restserver/controllers/buscar.js
+++ 07-restserver/controllers/buscar.js
@@ -1,13 +1,64 @@
-const { response } = require("express");
+const { response } = require("express")
+const { isValidObjectId } = require("mongoose")
+const { Usuario } = require("../models")
 
-const buscar = (req, res = response) => {
+const coleccionesPermitidas = [
+    'usuarios',
+    'categorias',
+    'productos',
+    'roles'
+]
+
+const buscarUsuario = async(termino = '', res = response) => {
+
+    const esMongoID = isValidObjectId(termino)
+
+    if(esMongoID) {
+        console.log('hola')
+        const usuario = await Usuario.findById(termino)
+        return res.json({
+            results: (usuario) ? [usuario] : []
+        })
+    }
+
+    const regex = new RegExp(termino, 'i')
+
+    const usuarios = await Usuario.find({
+        $or: [{nombre: regex}, {correo: regex}],
+        $and: [{estado: true}]
+    })
+
+    res.json({
+        resresults: usuarios
+    })
+
+}
+
+const buscar = async (req, res = response) => {
 
     const {coleccion, termino} = req.params
 
-    res.json({
-        coleccion,
-        termino
-    })
+    if(!coleccionesPermitidas.includes(coleccion)) {
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son: ${coleccionesPermitidas}`
+        })
+    }
+
+    switch(coleccion) {
+        case 'usuarios':
+            await buscarUsuario(termino, res)
+            break;
+        case 'categorias':
+            break;
+        case 'productos':
+            break;
+        case 'role':
+            break;
+        default:
+            res.status(500).json({
+                msg: 'Se me olvido hacer esta busqueda'
+            })
+    }
 }
 
 module.exports = {
