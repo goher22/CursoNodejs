71e8ac4e8f5d4a2ead7bbb8ae3af51926a533871
Carlos <gohercarlos@gmail.com>
2021-10-02 02:32:10 -0500

    Servicio para mostrar imagenes

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -80,7 +80,58 @@ const actualizarImagen = async (req, res = response) => {
 
 }
 
+const mostrarImagen = async (req, res = response) =>{
+
+  const {id, coleccion} = req.params
+
+  let modelo;
+
+  switch (coleccion) {
+    case 'usuarios':
+      modelo = await Usuario.findById(id)
+      if(!modelo) {
+        return res.status(400).json({
+          msg: `No existe un usuario con el id ${id}`
+        })
+      }
+      break;
+    case 'productos':
+      modelo = await Producto.findById(id)
+      if(!modelo) {
+        return res.status(400).json({
+          msg: `No existe un producto con el id ${id}`
+        })
+      }
+      break;
+    
+    default:
+      return res.status(500).json({msg: 'Se me olvido validar esto'})
+  }
+
+  try {
+
+    //Limpiar imagenes previas
+    if(modelo.img){
+      
+      // Hay que borrar la imgane del servidor 
+      const payhImagen = path.join(__dirname, '../uploads', coleccion, modelo.img)
+      if(fs.existsSync(payhImagen)) {
+        return res.sendFile(payhImagen)
+      }
+
+    }
+
+    res.json({msg: 'Falta place holder'})
+
+  } catch (msg) {
+    console.log(msg)
+    res.status(400).json({msg})    
+  }
+
+}
+
 module.exports = {
     cargarArchivo,
-    actualizarImagen
+    actualizarImagen,
+    mostrarImagen
 }
--- 07-restserver/routes/uploads.js
+++ 07-restserver/routes/uploads.js
@@ -1,6 +1,6 @@
 const {Router} = require('express')
 const { check } = require('express-validator')
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads')
+const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads')
 const { coleccionesPermitidas } = require('../helpers')
 const { validarCampos, validarArchivoSubir } = require('../middelwares')
 
@@ -14,6 +14,12 @@ router.put('/:coleccion/:id', [
     check('coleccion').custom(c=>coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
 ], actualizarImagen)
+
+router.get('/:coleccion/:id', [
+    check('id', 'El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom(c=>coleccionesPermitidas(c, ['usuarios', 'productos'])),
+    validarCampos
+], mostrarImagen)
 
 
 module.exports = router
