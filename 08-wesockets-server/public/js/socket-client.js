219fa3deb7976b7925e1d4875827a091cc55c8b1
Carlos <gohercarlos@gmail.com>
2021-10-02 17:14:25 -0500

    Mensaje de conexion y desconecion - Cliente

--- 08-wesockets-server/public/js/socket-client.js
+++ 08-wesockets-server/public/js/socket-client.js
@@ -1,2 +1,18 @@
 
+//referencias del html
+const lblOnline = document.getElementById("lblOnline")
+const lblOffline = document.getElementById("lblOffline")
+
 const socket = io()
+
+socket.on('connect', () => {
+    console.log("Conectado")
+    lblOffline.style.display = 'node'
+    lblOnline.style.display = ''
+})
+
+socket.on('disconnect', () => {
+    console.log("Desconenctado  ")
+    lblOnline.style.display = 'node'
+    lblOffline.style.display = ''
+})
