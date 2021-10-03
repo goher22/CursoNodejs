dbd91cacac979eaed1bf5fa53a2e14acf9b7b8c4
Carlos <gohercarlos@gmail.com>
2021-10-03 05:16:19 -0500

    Configuracion Socker.io

--- 10-socket-chat/models/server.js
+++ 10-socket-chat/models/server.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
 const { dbConnection } = require('../database/config');
+const { socketController } = require('../sockets/controller');
 
 class Server {
 
     constructor() {
         this.app  = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT
+        this.server = require('http').createServer(this.app)
+        this.io = require('socket.io')(this.server)
 
         this.paths = {
             auth:       '/api/auth',
@@ -53,6 +56,8 @@ class Server {
             createParentPath: true
         }));
 
+        //Sockets
+        this.sockets()
     }
 
     routes() {
@@ -66,8 +71,12 @@ class Server {
         
     }
 
+    sockets(){
+        this.io.on("connection", socketController)
+    }
+
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
