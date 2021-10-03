d0b29302a49b9a92efd7a43e04488ce9e18068b4
Carlos <gohercarlos@gmail.com>
2021-10-03 02:17:49 -0500

    Socket: Siguiente Ticket

--- 09-socket-colas/sockets/controller.js
+++ 09-socket-colas/sockets/controller.js
@@ -4,12 +4,14 @@ const ticketControl = new TicketControl()
 
 const socketController = (socket) => {
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
-        
-        const id = 123456789;
-        callback( id );
+    socket.emit('ultimo-ticket', ticketControl.ultimo)
 
-        socket.broadcast.emit('enviar-mensaje', payload );
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+        
+        const siguiente = ticketControl.siguiente()
+        callback(siguiente)
+
+        // TODO: Notificar que hay un ticket pendiente de asignar
 
     })
 
