89386848b3187355524d00c3434b1dd235fb4fa4
Carlos <gohercarlos@gmail.com>
2021-10-09 01:40:19 -0500

    Validar Token - Frontend

--- 10-socket-chat/public/js/chat.js
+++ 10-socket-chat/public/js/chat.js
@@ -1,3 +1,35 @@
+const { header } = require("express-validator")
+
+let usuario = null
+let socket = null
+
+//Validar el token del localStorage
+const validarJWT = async() => {
+    const token = localStorage.getItem('token') || ''
+    if(token.length <= 10) {
+        window.location = 'index.html'
+        throw new Error('No hay token en el servidor')
+    }
+
+    const resp = await fetch('http://localhost:8080/api/auth/',{
+        headers: {'x-token': token}
+    })
+
+    const {usuario: userDB, token: tokenDB} = await resp.json()
+
+    localStorage.setItem('token', tokenDB)
+    usuario.userDB
+
+}
+
+const main = async () => {
+
+    //Validar JWT
+    await validarJWT()
 
 
-const socket = io()
+}
+
+main()
+
+//const socket = io()
